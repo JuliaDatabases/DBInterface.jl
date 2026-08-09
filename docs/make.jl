7a841d9a03f93f9235cc349d5044a2c3e3f378d7
Jacob Quinn <quinn.jacobd@gmail.com>
2026-08-09 17:38:29 -0600

    fix(docs): support current Documenter releases

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,13 @@ using Documenter, DBInterface
 
 makedocs(;
     modules=[DBInterface],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(repolink="https://github.com/JuliaDatabases/DBInterface.jl"),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://github.com/JuliaDatabases/DBInterface.jl/blob/{commit}{path}#L{line}",
     sitename="DBInterface.jl",
     authors="Jacob Quinn",
-    assets=String[],
 )
 
 deploydocs(;
